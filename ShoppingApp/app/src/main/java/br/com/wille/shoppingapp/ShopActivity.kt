1c663bce2c52f38e4d32cce4789239122f3a2683
Leonardo Wille <leowille97@gmail.com>
2018-09-13 09:07:27 -0300

    NavigationDrawer ShoppingApp

--- ShoppingApp/app/src/main/java/br/com/wille/shoppingapp/ShopActivity.kt
+++ ShoppingApp/app/src/main/java/br/com/wille/shoppingapp/ShopActivity.kt
@@ -1,7 +1,9 @@
 package br.com.wille.shoppingapp
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 
 class ShopActivity : AppCompatActivity() {
 
@@ -11,5 +13,20 @@ class ShopActivity : AppCompatActivity() {
         setTheme(R.style.AppTheme)
 
         setContentView(R.layout.activity_shop)
+
+    }
+
+
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        if (item?.itemId == R.id.nav_notification){
+            startActivity(Intent(this, NotificationActivity::class.java))
+            return true
+        } else if (item?.itemId == R.id.nav_inbox){
+            startActivity(Intent(this, InboxActivity::class.java))
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
